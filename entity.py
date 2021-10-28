769df854ce088b8a640ca5fdb01bdb8588c83582
minsu.kim <minsu.kim@mytum.com>
2021-10-28 13:44:47 +0900

    ner tagger

--- entity.py
+++ entity.py
@@ -1,107 +1,76 @@
-import datetime
-
-import datefinder
-import dateutil.parser as dp
-
 import re
 import spacy
 import pandas as pd
+from spacy.training import Example
+
 data = pd.read_csv("inbound.csv", encoding='latin')
 data = data['CONTENT']
 SPACY_LOAD = spacy.load('C:/Users/trumpia/anaconda3/Lib/site-packages/en_core_web_sm/en_core_web_sm-3.1.0')
 
+#REGEX
+EMAIL_REGEX = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+ADDRESS_REGEX = re.compile(r'\d{3,5}(.){15,}\D\d{5}')
+URL_REGEX = re.compile(r'http\S{7,}')
+DATE_REGEX1 = re.compile(r'((0)?[1-9]|1[012])([-/ .])(3[01]|[12][0-9]|(0)?[1-9])')
+DATE_REGEX2 = re.compile(r'((0)?[1-9]|1[012])([-/ .])(3[01]|[12][0-9]|(0)?[1-9])([-/ .])(([1-9][0-9]{3})|([0-9]{2}))')
+TIME_REGEX = re.compile(r'^((([0]?[1-9]|1[0-2])(:|\.)?[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$')
+NEEDS = "PERSON|DATE|TIME|CARDINAL|ORDINAL"
 
 
 
-def refine_pattern_start_end(pattern: str) -> str:
-    """
-    The updated Regex pattern doesn't allow alpha characters prepended or appended
-    to the match.
-    :param pattern: monday|tuesday
-    :return: (?<![^\W\d_])monday(?![^\W\d_])|(?<![^\W\d_])tuesday(?![^\W\d_])
-    """
-    return '|'.join([fr'(?<![^\W\d_]){p}(?![^\W\d_])' for p in pattern.split('|')])
-
-DAYS_PATTERN = 'monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tue|tues|wed|thur|thurs|fri|sat|sun'
-DAYS_PATTERN = refine_pattern_start_end(DAYS_PATTERN)
-MONTHS_PATTERN = 'january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|sept|oct|nov|dec'
-MONTHS_PATTERN = refine_pattern_start_end(MONTHS_PATTERN)
-NUM_PATTERN = r'[1-3]?[0-9]'
 
 
+
+def URL_EXTRACTOR(str):
+    search = re.search(URL_REGEX, str)
+    if search != None:
+        result = search.group()
+        return result
+    else:
+        return None
+
+def ADDRESS_EXTRACTOR(str):
+    search = re.search(ADDRESS_REGEX, str)
+    if search != None:
+        result = search.group()
+        return result
+    else:
+        return None
 
 
 
 for i in range(len(data)):
-    
-    ORIGINAL_DATA = data[i]
-    SPACY_DATA = SPACY_LOAD(data[i])
 
-    CHECKER = 0
+    if URL_EXTRACTOR(data[i]) != None:
+        URL = URL_EXTRACTOR(data[i])
+        data[i] = data[i].replace(URL, "")
 
-    for token in SPACY_DATA:
-        if (token.ent_type_ == 'DATE') or (token.ent_type_ == 'CARDINAL'):
-            print(f"{token} ==> {token.ent_type_}")
-            CHECKER += 1
+    if ADDRESS_EXTRACTOR(data[i]) != None:
+        ADDRESS = ADDRESS_EXTRACTOR(data[i])
+        data[i] = data[i].replace(ADDRESS, "")
 
-    if CHECKER != 0:
 
-        NUM_FIND = re.findall(NUM_PATTERN, ORIGINAL_DATA)
-        MONTHS_FIND = re.findall(MONTHS_PATTERN, ORIGINAL_DATA)
-        DAYS_FIND = re.findall(DAYS_PATTERN, ORIGINAL_DATA)
 
-        matches = datefinder.find_dates(ORIGINAL_DATA)
-        LISTED_MATCH = list(matches)
-        
-        if len(NUM_FIND) == 1:
-            
-            if len(MONTHS_FIND) > 0:
+    doc = SPACY_LOAD(data[i])
+    print('---------------------------------')
+    print("Data No. {0}".format(i))
+    print("<-- Original = " + data[i] + " -->")
 
-                if len(LISTED_MATCH) != 0:
-                    
-                    for i in reversed(range(len(LISTED_MATCH))):
-                        MATCH_YEAR = LISTED_MATCH[i].year
-                        
-                        if not(1970 < MATCH_YEAR < 2025):
-                            del LISTED_MATCH[i]
-                    print('----------------')
-                    print("Data number ", i + 2)
-                    print("Original data : ", data[i])
-                    for match in matches:
-                        print("match found :", match)
-                    print('----------------')
-                
-                else:
-                    print('----------------')
-                    print("Original data : ", data[i])
-                    print("DATETIME NOT FOUND.")
-                    print('----------------')
-                    
-            else:
-                print('----------------')
-                print("Original data : ", data[i])
-                print("DATETIME NOT FOUND.")
-                print('----------------')
-    
-        else:
-            
-            matches = datefinder.find_dates(ORIGINAL_DATA)
-            
-            if len(LISTED_MATCH) != 0:
-                
-                print('----------------')
-                print("Data number ", i+2)
-                print("Original data : ", data[i])
-                for match in matches:
-                    print("match found :", match)
-                print('----------------')
-                
-            else:
-                print('----------------')
-                print("Original data : ", data[i])
-                print("DATETIME NOT FOUND.")
-                print('----------------')
 
-DIGITS_MODIFIER_PATTERN = r'\d+st|\d+th|\d+rd|first|second|third|fourth|fifth|sixth|seventh|eighth|nineth|tenth|next|last'
-DIGITS_PATTERN = r'\d{1,4}'
+    for e in doc:
+
+        TYPE = e.ent_type_
+
+        if (DATE_REGEX1.match(str(e)) != None) or (DATE_REGEX2.match(str(e)) != None):
+            TYPE = "DATE"
+
+        if (TIME_REGEX.match(str(e)) != None):
+            TYPE = "TIME"
+
+        #DATE 중에서 only digit, 길이 6 이하 제한, yyyymmdd 포맷 안지켜진거 다 태그 제거
+        #OCT 20 같은거 처리
+        #PERSON 도 조금만 더 올릴 방법 없나
+
+        if (TYPE in NEEDS) and (TYPE != ""):
+            print(f"{e} ==> {TYPE}")
 
