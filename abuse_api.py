8acf3d8034946ba089cfd0bf75d7c704ed2414a2
minsu.kim <minsu.kim@mytum.com>
2021-11-05 12:25:36 +0900

    abuse_api

--- abuse_api.py
+++ abuse_api.py
@@ -5,19 +5,12 @@ from nltk.corpus import stopwords
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re, pickle
 import spacy
-
-SPACY_LOAD = spacy.load('C:/Users/trumpia/anaconda3/Lib/site-packages/en_core_web_sm/en_core_web_sm-3.1.0')
-
-stop_words = set(stopwords.words('english'))
+import abuse_base as ab
 
 
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-
-loaded_model = load_model('best_model.h5')
 
 
-loaded_model.summary()
+
 
 app = Flask(__name__)
 
@@ -28,6 +21,7 @@ def mainpage():
 
 @app.route('/predict', methods=['POST'])
 def abuse_predict():
+
     print('prediction api called.')
     parser = reqparse.RequestParser()
     parser.add_argument('content')#json key 추가
@@ -37,48 +31,20 @@ def abuse_predict():
     sentence = sentence.replace("Org Name:", "")
     original = sentence
 
+
     print(sentence)
+    MODEL, BOW = ab.Load_Model('Abuse_Detect.h5', 'Abuse_Tokenizer.pickle')
+    tokens = ab.Preprocess_Predict(sentence)
 
-
-    sentence = sentence.lower()  # 소문자로 변환
-    tokens = sentence.split(' ')  # 공백 기준 스플릿
     token_score = []
-
-    for i in reversed(range(len(tokens))):
-        if '@' in tokens[i]:
-            del tokens[i]
-        elif '&' in tokens[i]:
-            del tokens[i]
-        elif 'http' in tokens[i]:
-            del tokens[i]
-        elif '#' in tokens[i]:
-            del tokens[i]
-        else:
-            tokens[i] = re.sub(r"[^a-zA-Z ]", " ", tokens[i])
-
-    tokens = ' '.join(tokens)
-    tokens = tokens.split(' ')
-
-    tokens = [word for word in tokens if not word in stop_words]  # 불용어 제거
-
-    for i in reversed(range(len(tokens))):
-        # 불용어 처리 이후 남은 null 제거
-        if tokens[i] == '':
-            del tokens[i]
-        # 길이 1인 찌꺼기 제거
-        elif len(tokens[i]) < 2:
-            del tokens[i]
-
-    print(tokens)
-
-    encoded = tokenizer.texts_to_sequences([tokens]) # 정수 인코딩
+    encoded = BOW.texts_to_sequences([tokens]) # 정수 인코딩
     pad_new = pad_sequences(encoded, 20) # 패딩
-    total_score = float(loaded_model.predict(pad_new)) # 예측
+    total_score = float(MODEL.predict(pad_new)) # 예측
 
     for i in range(len(tokens)):
-        encoded = tokenizer.texts_to_sequences([tokens[i]])  # 정수 인코딩
+        encoded = BOW.texts_to_sequences([tokens[i]])  # 정수 인코딩
         pad_new = pad_sequences(encoded, 20)  # 패딩
-        score = float(loaded_model.predict(pad_new))
+        score = float(MODEL.predict(pad_new))
         score = str(round(score * 100, 2))# 예측
         token_score.append(score)
 
