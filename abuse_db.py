7b642928bdb6bdd92f1a4d99624a29c80187084a
alstn0723 <minsu.kim@mytrum.com>
2021-11-10 13:28:44 +0900

    db stackin' api

--- abuse_db.py
+++ abuse_db.py
@@ -1,10 +1,11 @@
-import flask_reqparse as reqparse
-from flask import Flask
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from flask import Flask, request
 import abuse_tester as at
 import oracle_conn as oc
 
-
+#DB구동
+oc.Oracle_Init()
+#MODEL 불러오기
+MODEL, BOW = at.Load_Model('Abuse_Detect.h5', 'Abuse_Tokenizer.pickle')
 
 app = Flask(__name__)
 
@@ -13,18 +14,15 @@ def mainpage():
     return 'Hi minsoo'
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict/oracle', methods=['POST'])
 def abuse_predict():
 
     print('prediction api called.')
-    parser = reqparse.RequestParser()
-    parser.add_argument('content')#json key 추가
-    parser.add_argument('memberUid')
-    parser.add_argument('userId')
 
-    sentence = parser.parse_args()['content']
-    member_Uid = parser.parse_args()['memberUid']
-    user_Uid = parser.parse_args()['userId']
+
+    sentence = request.json.get('content')
+    member_Uid = request.json.get('memberUid')
+    user_Uid = request.json.get('userId')
 
     org_idx = sentence.find(":")
 
@@ -35,26 +33,12 @@ def abuse_predict():
         sentence = sentence[org_idx:]
 
     original = sentence
-
-
     print(sentence)
-    MODEL, BOW = at.Load_Model('Abuse_Detect.h5', 'Abuse_Tokenizer.pickle')
-    tokens = at.Preprocess_Predict(sentence)
 
-    token_score = []
-    encoded = BOW.texts_to_sequences([tokens]) # 정수 인코딩
-    pad_new = pad_sequences(encoded, 20) # 패딩
-    total_score = float(MODEL.predict(pad_new)) # 예측
+    #예측 진행
+    sentence, total_score, tokens, token_score = at.ACC_Check(sentence, MODEL, BOW)
 
-    for i in range(len(tokens)):
-        encoded = BOW.texts_to_sequences([tokens[i]])  # 정수 인코딩
-        pad_new = pad_sequences(encoded, 20)  # 패딩
-        score = float(MODEL.predict(pad_new))
-        score = str(round(score * 100, 2))# 예측
-        token_score.append(score)
-
-    oc.Oracle_Init()
-    #Connection, Cursor 받아오기
+    # Connection, Cursor 받아오기
     conn, cur = oc.Oracle_Conn("d_alstn0723", "steam9588!", "192.168.2.101:1521/DCDB")
 
     #CONTENT TABLE
@@ -66,15 +50,19 @@ def abuse_predict():
         sql = """INSERT INTO E_TOKEN_SPAM_SCORE(TOKENSPAMSCOREUID, CONTENTSPAMSCOREUID, TOKEN, SCORE) VALUES (TOKEN_SPAM_SCORE_SEQ.NEXTVAL, CONTENT_SPAM_SCORE_SEQ.CURRVAL, :1, :2)"""
         cur.execute(sql, (tokens[j], token_score[j]))
 
-    #CONTENT_SPAM_SCORE_SEQ.CURRVAL 꺼내서 json 리턴 해야됨
+    sql = """SELECT CONTENT_SPAM_SCORE_SEQ.CURRVAL FROM E_CONTENT_SPAM_SCORE"""
+    cur.execute(sql)
 
-    #DB에 저장 후 종료
+    #CONTENTSPAMSCOREUID => PRIMARY KEY
+    CSSU = cur.fetchall()[0][0]
+    CSSU = {"contentSpamScoreUid":CSSU}
+
+    #DB에 저장 후 DB 연결 종료
     oc.Close_Conn(conn)
 
-
-    return 200
+    return CSSU
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.3.184', port=5000)
