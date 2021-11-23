f852574f46beba5f32e57e72a18f41b4f99f5edd
alstn0723 <minsu.kim@mytrum.com>
2021-11-23 17:15:20 +0900

    for new dataset

--- bertspamtest.py
+++ bertspamtest.py
@@ -1,39 +1,31 @@
 import tensorflow_hub as hub
 import pandas as pd
 import tensorflow_text as text
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
-df = pd.read_csv('dataset/spam_data.csv')
 
+df = pd.read_csv('dataset/spam_data.csv', encoding='latin')
+del df['Unnamed: 0']
+df = df.reset_index(drop=True)
 print(df)
 
-#분리
-df_spam = df[df['Category']=='spam']
-df_ham = df[df['Category']=='ham']
 
-print("Ham Dataset Shape:", df_ham.shape)
 
-print("Spam Dataset Shape:", df_spam.shape)
-
-df_ham_downsampled = df_ham.sample(df_spam.shape[0])
-df_ham_downsampled.shape
-
-#반반 맞춘 ㅈ만한 데이터
-df_balanced = pd.concat([df_spam , df_ham_downsampled])
-
-print(df_balanced['Category'].value_counts())
-df_balanced.sample(10)
+print(df['SPAM'].value_counts())
+df.sample(10)
 
 #spam 1 ham 0
-df_balanced['spam'] = df_balanced['Category'].apply(lambda x:1 if x=='spam' else 0)
-df_balanced.sample(4)
+
 
 from sklearn.model_selection import train_test_split
-X_train, X_test , y_train, y_test = train_test_split(df_balanced['Message'], df_balanced['spam'], stratify = df_balanced['spam'])
+X_train, X_test , y_train, y_test = train_test_split(df['CONTENT'], df['SPAM'], test_size=0.2)
+print(len(X_train))
+print(len(y_train))
+print(len(X_test))
+print(len(y_test))
+
 
 bert_preprocessor = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3')
 bert_encoder = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4')
